db6f7f0127d890e5f0766d20fa2a3d4a93dcb2a6
reyhaneh <forrgh76@gmail.com>
2025-08-22 15:46:07 +0330

    rename variable to be meaningful

--- core/main.py
+++ core/main.py
@@ -23,9 +23,9 @@ async def retrieve_data(min: Optional[float] = Query(default=None), max:Optional
 
 @app.get("/expenses/{id}", status_code=status.HTTP_200_OK)
 async def get_expense(id:int):
-    for e in Expenses:
-        if(e["id"] == id):
-            return e
+    for item in Expenses:
+        if(item["id"] == id):
+            return item
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
 
 
@@ -40,31 +40,31 @@ async def create_expense(
 
 @app.put("/expenses/{id}", status_code=status.HTTP_200_OK)
 async def replace_expense(id:int, description: str = Body(...), amount: float  = Body(...)):
-    for e in Expenses:
-        if(e["id"] == id):
-            e["description"] = description
-            e["amount"] = amount
-            return e
+    for item in Expenses:
+        if(item["id"] == id):
+            item["description"] = description
+            item["amount"] = amount
+            return item
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
 
 
 @app.patch("/expenses/{id}", status_code=status.HTTP_200_OK)
 async def update_expense(id:int, description:  Optional[str] = Body(None), amount:  Optional[float] = Body(None) ):
-    for e in Expenses:
-        if(e["id"] == id):
+    for item in Expenses:
+        if(item["id"] == id):
             if description is not None and description.strip() != "":
-                e["description"] = description
+                item["description"] = description
             if amount is not None:
-                e["amount"] = amount
-            return e
+                item["amount"] = amount
+            return item
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
 
 
 @app.delete("/expenses/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_expense(id: int):
-    for i, n in enumerate(Expenses):
-        if n["id"] == id:
-            del Expenses[i]
+    for index, item in enumerate(Expenses):
+        if item["id"] == id:
+            del Expenses[index]
             return  {"message": f"Name with ID {id} deleted successfully"}
             # return JSONResponse(content = {"message": f"Name with ID {id} deleted successfully"}, status_code=status.HTTP_200_ok)
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
